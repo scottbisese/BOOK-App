ae9d7184a932e1cf047722095868647e8a04d891
scottbisese <bisese.scott@gmail.com>
2019-08-27 09:18:34 -0500

    starter code added and scaffolded

---
+++ lab/starter-code/server.js
@@ -0,0 +1,52 @@
+'use strict';
+
+// Application Dependencies
+const express = require('express');
+const superagent = require('superagent');
+
+// Application Setup
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Application Middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+// Set the view engine for server-side templating
+app.set('view engine', 'ejs');
+
+// API Routes
+// Renders the search form
+app.get('/', (req, res) => { 
+  // Note that .ejs file extension is not required
+  response.render('pages/index');
+});
+
+// Creates a new search to the Google Books API
+app.post('/searches', createSearch);
+
+// Catch-all
+app.get('*', (request, response) => response.status(404).send('This route does not exist'));
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
+// HELPER FUNCTIONS
+function Book(info) {
+  //Finish this constructor function
+}
+
+// No API key required
+function createSearch(request, response) {
+  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+
+  console.log(request.body);
+  console.log(request.body.search);
+
+  if (request.body.search[1] === 'title') { url += `+intitle:${request.body.search[0]}`; }
+  if (request.body.search[1] === 'author') { url += `+inauthor:${request.body.search[0]}`; }
+
+  superagent.get(url)
+    .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
+    .then(results => response.render('pages/searches/show', {searchResults: results}));
+  // how will we handle errors?
+}
