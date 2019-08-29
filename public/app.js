5e590eee0b72d497c2810311b6e9a564e01be747
scottbisese <bisese.scott@gmail.com>
2019-08-29 14:01:47 -0500

    css stuff

--- public/app.js
+++ public/app.js
@@ -2,5 +2,5 @@ $('#navButton').click((event) => {
   window.location.href = '/';
 });
 $('.show-form').click((event) => {
-  $('.show-form').siblings('form').slideToggle();
+  $('.show-form').siblings('form').slideToggle(666);
 });
